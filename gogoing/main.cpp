9b30ff08a0a742745e52759896f16ca033f26c61
godoing <20141105063T@mac04.local>
2015-11-18 21:12:52 +0800

    NEW－point
    正确的指针文件

--- gogoing/main.cpp
+++ gogoing/main.cpp
@@ -1,3 +1,4 @@
+
 //
 //  main.cpp
 //  gogoing
@@ -10,6 +11,10 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int a,*p;
+    a=100;
+    p=&a;
+    printf("a=%x &a=%x &p=%x p=%x *p=%x\n",a,&a,&p,p,*p);
+    //std::cout << "Hello, World!\n";
     return 0;
 }
